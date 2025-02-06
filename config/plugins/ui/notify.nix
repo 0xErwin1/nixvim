bf0e38fd3cbb2dc88d2b62b1ae45ed5fade3f00c
Ignacio Perez <ignacio.perez2123@protonmail.com>
2025-02-06 11:12:08 -0300

    Fix warnings

--- config/plugins/ui/notify.nix
+++ config/plugins/ui/notify.nix
@@ -1,11 +1,14 @@
 {
   plugins.notify = {
     enable = true;
-    fps = 10;
-    render = "minimal";
-    stages = "fade_in_slide_out";
-    timeout = 4000;
-    topDown = false;
-    backgroundColour = "#000000";
+    settings = {
+
+      fps = 10;
+      render = "minimal";
+      stages = "fade_in_slide_out";
+      timeout = 4000;
+      topDown = false;
+      backgroundColour = "#000000";
+    };
   };
 }
