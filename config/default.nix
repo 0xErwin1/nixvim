162b4c11c7e98b692580ccd50048a9c75e578216
Ignacio Perez <ignacio.perez2123@protonmail.com>
2025-06-16 21:43:09 +0100

    Allow languages

--- config/default.nix
+++ config/default.nix
@@ -4,13 +4,13 @@
     ./basic.nix
     ./plugins/languages/typescript.nix
     ./plugins/languages/rust.nix
-    # ./plugins/languages/ocaml.nix
+    ./plugins/languages/ocaml.nix
     ./plugins/languages/c.nix
-    # ./plugins/languages/python.nix
+    ./plugins/languages/python.nix
     ./plugins/languages/zig.nix
     ./plugins/languages/docker.nix
     ./plugins/languages/go.nix
     ./plugins/languages/perl.nix
-    #./plugins/languages/java.nix
+    ./plugins/languages/java.nix
   ];
 }
